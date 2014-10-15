fb93e28a156925fcc48186f45305372958c29bf9
Ewoud Kohl van Wijngaarden <ekohlvanwijngaarden@antagonist.nl>
2014-10-15 10:40:18 +0200

    Move all OS-dependent config to params.pp
    This also treats the entire RedHat osfamily (except Fedora) as EL.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,15 +1,6 @@
 # Katello Install
 class katello::install {
-  if ($::osfamily == 'RedHat' and $::operatingsystem != 'Fedora'){
-    $os = 'RHEL'
-  } else {
-    $os = 'Fedora'
-  }
-  $rubygem = $os ? {
-    'RHEL'   => 'ruby193-rubygem-katello',
-    'Fedora' => 'rubygem-katello'
-  }
-  package{['katello', $rubygem]:
+  package { $katello::package_names:
     ensure => installed,
   }
 }
