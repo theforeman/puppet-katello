406a6f122d18490dfb4752b744cf7a6f8d35aa5e
Stephen Benjamin <stephen@bitbin.de>
2016-11-10 19:00:54 -0500

    Crane moved back to capsule for now (#153)

--- manifests/init.pp
+++ manifests/init.pp
@@ -123,7 +123,6 @@ class katello (
     manage_httpd           => false,
     manage_plugins_httpd   => true,
     manage_squid           => true,
-    enable_crane           => true,
     enable_rpm             => true,
     enable_puppet          => true,
     enable_docker          => true,
