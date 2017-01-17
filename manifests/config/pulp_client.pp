8ec320741030a2962fe5c4d14c06f1decee2263b
Tyler Wick <twick@apptio.com>
2017-01-17 09:46:21 -0800

    Config entry for pulp oauth

--- manifests/config/pulp_client.pp
+++ manifests/config/pulp_client.pp
@@ -1,5 +1,11 @@
 #Katello Pulp Client config
 class katello::config::pulp_client {
+  foreman_config_entry { 'use_pulp_oauth':
+    value          => 'true',
+    ignore_missing => false,
+    before         => Exec['foreman-rake-db:seed'],
+  }
+
   foreman_config_entry { 'pulp_client_cert':
     value          => '/etc/pki/pulp/cert.pem',
     ignore_missing => false,
