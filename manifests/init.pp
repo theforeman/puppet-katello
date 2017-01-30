6ac1610d61b4aab2e6098d45233c54e868b431d5
Stephen Benjamin <stephen@redhat.com>
2017-01-30 15:50:21 -0500

    Verify pulp CA against server CA

--- manifests/init.pp
+++ manifests/init.pp
@@ -90,7 +90,7 @@ class katello (
   validate_absolute_path($repo_export_dir)
 
   $candlepin_ca_cert = $::certs::ca_cert
-  $pulp_ca_cert = $::certs::ca_cert
+  $pulp_ca_cert = $::certs::katello_server_ca_cert
 
   Class['certs'] ~>
   class { '::certs::apache': } ~>
