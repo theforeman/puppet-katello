40ababa0dcef54808349ac7d718b07a54f896a3f
Eric D. Helms <ericdhelms@gmail.com>
2021-02-10 16:43:08 -0500

    No need to configure qpid::client

--- spec/classes/qpid_spec.rb
+++ spec/classes/qpid_spec.rb
@@ -5,30 +5,28 @@ describe 'katello::qpid' do
     context "on #{os}" do
       let(:facts) { facts }
 
-      if facts[:operatingsystemmajrelease] == '7'
-        context 'with default parameters' do
-          it { is_expected.to compile.with_all_deps }
-          it { is_expected.to contain_class('certs::qpid').that_notifies(['Service[qpidd]', 'Class[qpid]']) }
-          it { is_expected.to create_class('qpid').with_wcache_page_size(4).with_interface('lo') }
+      context 'with default parameters' do
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_class('certs::qpid').that_notifies(['Service[qpidd]', 'Class[qpid]']) }
+        it { is_expected.to create_class('qpid').with_wcache_page_size(4).with_interface('lo') }
+      end
+
+      it do
+        is_expected.to create_qpid__config__queue('katello.agent')
+          .with_ssl_cert('/etc/pki/katello/certs/foo.example.com-qpid-broker.crt')
+          .with_ssl_key('/etc/pki/katello/private/foo.example.com-qpid-broker.key')
+      end
+
+      context 'with overridden parameters' do
+        let :params do
+          {
+            wcache_page_size: 8,
+          }
         end
 
-        it do
-          is_expected.to create_qpid__config__queue('katello.agent')
-            .with_ssl_cert('/etc/pki/katello/certs/foo.example.com-qpid-broker.crt')
-            .with_ssl_key('/etc/pki/katello/private/foo.example.com-qpid-broker.key')
-        end
-
-        context 'with overridden parameters' do
-          let :params do
-            {
-              wcache_page_size: 8,
-            }
-          end
-
-          it 'should pass the variable' do
-            is_expected.to compile.with_all_deps
-            is_expected.to create_class('qpid').with_wcache_page_size(8)
-          end
+        it 'should pass the variable' do
+          is_expected.to compile.with_all_deps
+          is_expected.to create_class('qpid').with_wcache_page_size(8)
         end
       end
     end
