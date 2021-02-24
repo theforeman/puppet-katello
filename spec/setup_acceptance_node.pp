d0342d3dd6eb8d7d40ee52f694bf6c988f4110b7
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-02-24 13:48:39 +0100

    Remove pki-core workaround
    This is now handled in the candlepin module.

--- spec/setup_acceptance_node.pp
+++ spec/setup_acceptance_node.pp
@@ -13,10 +13,6 @@ group { 'foreman':
 }
 
 if $facts['os']['release']['major'] == '8' {
-  # https://tickets.puppetlabs.com/browse/PUP-9978
-  exec { '/usr/bin/dnf -y module enable pki-core':
-  }
-
   package { 'glibc-langpack-en':
     ensure => installed,
   }
